31ddc7e94ec44716c95c6623805a0c362a206156
miguel aguirre <miguelaguirreleon@gmail.com>
2021-09-06 10:23:32 -0500

    m

--- ReadAllPorts.js
+++
@@ -1,6 +0,0 @@
-const SerialPort = require("serialport");
-const Readline = require("@serialport/parser-readline");
-const p = require("./Get_Puerto.js");
-pp = p.getName.then(function (path) {
-  console.log(path);
-});
